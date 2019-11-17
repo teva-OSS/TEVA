9f6c543595dd947d4eb357282b52a339fbee1cf7
Kate <o.keito317@gmail.com>
2019-11-17 20:32:33 +0900

    add drag and drop sample

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -14,6 +14,14 @@ module.exports = {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             }
         ]
     }
