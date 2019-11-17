458735aa83aa537b57d27e16e9a0136d4e4a5c24
Kate <o.keito317@gmail.com>
2019-11-17 19:27:54 +0900

    Add json receive method

--- server.py
+++ server.py
@@ -1,2 +1,16 @@
+from tornado import ioloop, web
+
+from src.handler import MainHandler
+
+
+def main_app():
+    return web.Application([
+        ('/', MainHandler),
+    ])
+
+
 if __name__ == '__main__':
-    print('Test')
+    print('Running at http://localhost:8080')
+    app = main_app()
+    app.listen(8080)
+    ioloop.IOLoop.current().start()
