2b8168e086bddad99647a71e712b2a2907629768
0Kate <o.keiti317@gmail.com>
2019-11-20 00:26:52 +0900

    add parsing (WIP)

--- src/handler.py
+++ src/handler.py
@@ -2,7 +2,18 @@ import json
 
 from tornado import web
 
+from resource import resource_map
+
 
 class MainHandler(web.RequestHandler):
     def post(self):
-        print(json.loads(self.request.body.decode()))
+        json_template = json.loads(self.request.body.decode())
+        print(json_template)
+
+        cloud = json_template['cloud']
+        resources_json = json_template['resources']
+
+        for resource_json in resources_json:
+            resource_type = resource_json['type']
+            resource = resource_map[cloud][resource_type](resource_json)
+            print(resource.resource_type)
